3a0ef55b7cd44661f9a24698e1ce658e21b8c437
amanda100929 <amanda.escobar.brito@escola.pr.gov.br>
2025-10-17 08:27:23 -0300

    as2

--- server/server/models/Rank.js
+++
@@ -1,9 +0,0 @@
-// server/models/Rank.js
-const mongoose = require('mongoose');
-
-const rankSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  score: { type: Number, required: true },
-});
-
-module.exports = mongoose.model('Rank', rankSchema);
